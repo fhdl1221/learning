8e435997c272b927ff647eacb96936c4a3f352e3
fhdl1221 <fhdl353212@gmail.com>
2025-09-18 15:57:43 +0900

    loop

---
+++ js/07-loop/main.js
@@ -0,0 +1,18 @@
+// 반복문이 없는 코드
+// 변수 number를 선언하고, 0을 할당
+// 변수 number를 3번 출력
+// 1씩 증가하면서
+let number = 0;
+
+console.log(number);
+number = number + 1;
+
+console.log(number);
+number = number + 1;
+
+console.log(number);
+number = number + 1;
+
+for (let number2 = 0; number2 < 3; number2++) {
+  console.log(number2);
+}
