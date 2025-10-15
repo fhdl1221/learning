7293553b19675d352ad9a2f18a80537f1745b7c6
fhdl1221 <fhdl353212@gmail.com>
2025-10-15 17:42:36 +0900

    비동기 처리와 사용자 인증

--- react/redux-app/src/main.jsx
+++ react/redux-app/src/main.jsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store/index.js";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/index.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
